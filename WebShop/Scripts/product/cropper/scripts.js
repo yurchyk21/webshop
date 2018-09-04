f1392dc5e8bd141be3200293267397dca7ec5008
novakvova <novakvova@gmail.com>
2018-09-04 13:04:50 +0300

    Cropper work is good oop

--- WebShop/Scripts/product/cropper/scripts.js
+++ WebShop/Scripts/product/cropper/scripts.js
@@ -1,70 +1,105 @@
 $(function () {
-    var dataCrop = false;
-    var $canvas = $("#canvas"),
-        context = $canvas.get(0).getContext('2d');
-    $("#dragModeMove").on("click", function () {
-        $('#canvas').cropper('setDragMode', 'move');
-    });
+    var cropper = function () {
+        var self = this;
+        var isCropped = false;
+        var dialog = {
+            body: document.querySelector('body'),
+            show: function () {
+                this.body.classList.toggle("open");
+                $(".containerCrop").show();
+                $(".navbar").hide();
+            },
+            hide: function () {
+                this.body.classList.remove("open");
+                $(".containerCrop").hide();
+                $(".navbar").show();
+            }
+        };
 
-    $("#dragModeCrop").on("click", function () {
-        $('#canvas').cropper('setDragMode', 'crop');
-    });
-    var x = 1;
-    $("#scaleX").on("click", function () {
-        $('#canvas').cropper('scaleX', x *= -1);
+        function init() {
+            initControls();
 
-    });
-    var y = 1;
-    $("#scaleY").on("click", function () {
-        $('#canvas').cropper('scaleY', y *= -1);
-    });
+            $("#cropperClose").on("click", dialog.hide());
+            $('#img_file').on('change', function () {
+                if (this.files && this.files[0]) {
+                    if (this.files[0].type.match(/^image\//)) {
+                        onLoad(this.files[0]);
+                    }
+                    else {
+                        alert("Invalid file type");
+                    }
+                }
+                else {
+                    alert("Please select a file.");
+                }
 
-    $("#rotateLeft").on("click", function () {
-        $('#canvas').cropper('rotate', 45);
-    });
+            });
+            $("#crop").click(function () {
+                cropped();
+            });
+            //private method
+            function initControls() {
 
-    $("#rotateRight").on("click", function () {
-        $('#canvas').cropper('rotate', -45);
-    });
+                $("#dragModeMove").on("click", function () {
+                    $('#canvas').cropper('setDragMode', 'move');
+                });
 
-    $("#moveLeft").on("click", function () {
-        $('#canvas').cropper('move', -50, 0);
-    });
+                $("#dragModeCrop").on("click", function () {
+                    $('#canvas').cropper('setDragMode', 'crop');
+                });
+                var x = 1;
+                $("#scaleX").on("click", function () {
+                    $('#canvas').cropper('scaleX', x *= -1);
 
-    $("#moveRight").on("click", function () {
-        $('#canvas').cropper('move', 50, 0);
-    });
+                });
+                var y = 1;
+                $("#scaleY").on("click", function () {
+                    $('#canvas').cropper('scaleY', y *= -1);
+                });
 
-    $("#moveUp").on("click", function () {
-        $('#canvas').cropper('move', 0, -50);
-    });
+                $("#rotateLeft").on("click", function () {
+                    $('#canvas').cropper('rotate', 45);
+                });
 
-    $("#moveDown").on("click", function () {
-        $('#canvas').cropper('move', 0, 50);
-    });
+                $("#rotateRight").on("click", function () {
+                    $('#canvas').cropper('rotate', -45);
+                });
 
-    $("#zoomPlus").on("click", function () {
-        $('#canvas').cropper('zoom', 0.1);
-    });
+                $("#moveLeft").on("click", function () {
+                    $('#canvas').cropper('move', -50, 0);
+                });
 
-    $("#zoomMinus").on("click", function () {
-        $('#canvas').cropper('zoom', -0.1);
-    });
+                $("#moveRight").on("click", function () {
+                    $('#canvas').cropper('move', 50, 0);
+                });
 
-    $("#cropperClose").on("click", cropperClose);
-    var body = document.querySelector('body');
-    function cropperClose() {
-        body.classList.remove("open");
-        $(".containerCrop").hide();
-        $(".navbar").show();
-    }
+                $("#moveUp").on("click", function () {
+                    $('#canvas').cropper('move', 0, -50);
+                });
 
+                $("#moveDown").on("click", function () {
+                    $('#canvas').cropper('move', 0, 50);
+                });
 
-    $('#img_file').on('change', function () {
-        if (this.files && this.files[0]) {
-            if (this.files[0].type.match(/^image\//)) {
+                $("#zoomPlus").on("click", function () {
+                    $('#canvas').cropper('zoom', 0.1);
+                });
+
+                $("#zoomMinus").on("click", function () {
+                    $('#canvas').cropper('zoom', -0.1);
+                });
+            }
+        }
+
+        function crop() {
+            init();
+            isCropped = true;   
+        }
+
+        function onLoad(fileImage) {
+                var $canvas = $("#canvas"),
+                    context = $canvas.get(0).getContext('2d');
                 var reader = new FileReader();
-                
                 reader.onload = function (e) {
                     var img = new Image();
                     img.onload = function () {
@@ -76,11 +111,8 @@ $(function () {
                             return;
                         }
                         //Показуємо діалог
-
-                        body.classList.toggle("open");
-                        $(".containerCrop").show();
-                        $(".navbar").hide();
-                        dataCrop = true;
+                        dialog.show();
+                        isCropped = true;
 
                         context.drawImage(img, 0, 0);
                         var cropper = $canvas.cropper('destroy').cropper({
@@ -104,43 +136,39 @@ $(function () {
                     };
                     img.src = e.target.result;
                 };
-                $("#crop").click(function () {
-                    if (dataCrop) {
-                        var croppedImage = $canvas.cropper('getCroppedCanvas').toDataURL('image/jpg');
-                        $('#result').html($('<img>').attr('src', croppedImage));
-                        console.log(croppedImage);
-                        //Зображення обрізане записуємо у скрите поле на формі
-                        var imageCurrent = "#containerimage" + CounterImages;
-                        var image = $(imageCurrent).find(".uploadimage")[0];
-                        image.src = croppedImage;
-                        //$("#imgSelectView").attr("src", croppedImage);
-                        $('#ImageBase64').attr("value", croppedImage.split(',')[1]);
-                        //Завантажили одне фото на сайт
-                        CounterImages++;
-                        var itemAddImage = '';
-                        itemAddImage += '<div class="col-md-2 plusupload" id="containerimage'
-                            + CounterImages + '">';
-                        itemAddImage += '<div class="thumbnail">';
-                        itemAddImage += '<img src="' + PathImage + '" class="uploadimage" alt = "Lights" style = "width:100%" >';
-                        itemAddImage += '<div class="caption"><p>Add New Image</p></div>';
-                        itemAddImage += '</div>';
-                        itemAddImage += '</div>';
+                reader.readAsDataURL(fileImage);
+            };
+        
+        function cropped() {
+            if (isCropped) {
+                var $canvas = $("#canvas");
+                var croppedImage = $canvas.cropper('getCroppedCanvas').toDataURL('image/jpg');
+                $('#result').html($('<img>').attr('src', croppedImage));
+                //console.log(croppedImage);
+                //Зображення обрізане записуємо у скрите поле на формі
+                var imageCurrent = "#containerimage" + CounterImages;
+                var image = $(imageCurrent).find(".uploadimage")[0];
+                image.src = croppedImage;
+                //$("#imgSelectView").attr("src", croppedImage);
+                $('#ImageBase64').attr("value", croppedImage.split(',')[1]);
+                //Завантажили одне фото на сайт
+                CounterImages++;
+                var itemAddImage = '';
+                itemAddImage += '<div class="col-md-2 plusupload" id="containerimage'
+                    + CounterImages + '">';
+                itemAddImage += '<div class="thumbnail">';
+                itemAddImage += '<img src="' + PathImage + '" class="uploadimage" alt = "Lights" style = "width:100%" >';
+                itemAddImage += '<div class="caption"><p>Add New Image</p></div>';
+                itemAddImage += '</div>';
+                itemAddImage += '</div>';
 
-                        $("#listUploadImages").append(itemAddImage);
-                        cropperClose();
-                        dataCrop = false;
-                    }
-                });
-
-                reader.readAsDataURL(this.files[0]);
-            }
-            else {
-                alert("Invalid file type");
+                $("#listUploadImages").append(itemAddImage);
+                dialog.hide();
+                isCropped = false;
             }
         }
-        else {
-            alert("Please select a file.");
-        }
+        return { crop: crop };
+    }();
 
-    });
+    cropper.crop();
 });
