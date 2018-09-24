03df0399cb5feebca08c65d540a038550a79bfca
Olhovik Irina <iratqsm@gmail.com>
2018-09-24 18:59:21 +0300

    ma commit

---
+++ WebShop/Migrations/201809201614457_tblProductImageBasket added..cs
@@ -0,0 +1,26 @@
+namespace WebShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tblProductImageBasketadded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tblProductImageBasket",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 150),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.tblProductImageBasket");
+        }
+    }
+}
