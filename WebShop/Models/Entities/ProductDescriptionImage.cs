f73016b0cb8250a65ebd35df1ff108d265b90384
Olhovik Irina <iratqsm@gmail.com>
2018-09-24 20:41:12 +0300

    ma commit2

---
+++ WebShop/Models/Entities/ProductDescriptionImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.Models.Entities
+{
+    [Table("tblProductDescriptionImages")]
+    public class ProductDescriptionImage
+    {
+        [Key]
+        [StringLength(maximumLength:150)]
+        public string Name { get; set; }
+        [ForeignKey("Product")]
+        public int? ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
